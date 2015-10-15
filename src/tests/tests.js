925b0f8fa0e0183762863de317fa0a1b93b41a90
Albert-Jan Nijburg <albertjan@curit.com>
2015-10-15 22:13:32 +0200

    normalizing paths

--- src/tests/tests.js
+++ src/tests/tests.js
@@ -268,6 +268,27 @@ assert time.time() > 0`, 'testmodule.py').then(() => vm.exec('import testmodule'
 import time
 time.sleep(1)
   `).then(() => vm.exec('import testerdetest'));
+}).
+then(() => {
+  return vm.exec(`
+import os
+
+print "Path at terminal when executing this file"
+print os.getcwd() + "\n"
+
+print "This file path, relative to os.getcwd()"
+print __file__ + "\n"
+
+print "This file full path (following symlinks)"
+full_path = os.path.realpath(__file__)
+print full_path + "\n"
+
+print "This file directory and name"
+path, file = os.path.split(full_path
+print path + ' --> ' + file + "\n"
+
+print "This file directory only"
+print os.path.dirname(full_path)`);
 })
 // Check that you can create additional VMs using `new`
 .then(() => {
