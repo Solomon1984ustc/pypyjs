8e8f5ace2c90ff738f7a5e642af03a510f772bdf
Albert-Jan Nijburg <albertjan@curit.com>
2015-10-15 22:19:23 +0200

    no newlines

--- src/tests/tests.js
+++ src/tests/tests.js
@@ -274,18 +274,18 @@ then(() => {
 import os
 
 print "Path at terminal when executing this file"
-print os.getcwd() + "\n"
+print os.getcwd()
 
 print "This file path, relative to os.getcwd()"
-print __file__ + "\n"
+print __file__
 
 print "This file full path (following symlinks)"
 full_path = os.path.realpath(__file__)
-print full_path + "\n"
+print full_path
 
 print "This file directory and name"
 path, file = os.path.split(full_path
-print path + ' --> ' + file + "\n"
+print path + ' --> ' + file
 
 print "This file directory only"
 print os.path.dirname(full_path)`);
